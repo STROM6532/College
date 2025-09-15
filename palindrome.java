e6ac8689606517ce6e6eafe2fd47a0f739486e10
ANIMESH SAHOO <stromcracker6532@gmail.com>
2025-09-15 15:50:54 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,29 @@
 
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome number.");
+        } else {
+            System.out.println(num + " is not a palindrome number.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int reversed = 0;
+        int original = num;
+
+        while (num != 0) {
+            int remainder = num % 10;
+            reversed = reversed * 10 + remainder;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
